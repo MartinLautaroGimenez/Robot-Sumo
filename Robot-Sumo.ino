e53d229c519157b7ef03c1e52b23aa24ddf75f1e
Martin Lautaro Gimenez <142127631+martinLgimenez@users.noreply.github.com>
2023-11-01 10:00:14 -0300

    Se desactivó la función de seguidor de linea. (Dia de la competencia)

--- Robot-Sumo.ino
+++ Robot-Sumo.ino
@@ -4,8 +4,6 @@ int motor_B1 = 11;
 int motor_B2 = 10;
 int sensor_adelante_trig = 9;
 int sensor_adelante_echo = 8;
-int seguidordelinealect;
-int seguidor_de_linea = 2; /*Violeta, gris blanco  */
 bool bandera = true;
 
 // by mario
@@ -25,8 +23,6 @@ void setup() {
   pinMode(sensor_adelante_echo, INPUT);
   digitalWrite(sensor_adelante_trig, LOW);
   Serial.begin(9600);
-  pinMode(seguidor_de_linea, INPUT);
-  attachInterrupt(digitalPinToInterrupt(seguidor_de_linea), funcbanderas, LOW);
 }
 
 void loop() {
@@ -41,20 +37,19 @@ void loop() {
     tiempo = pulseIn(sensor_adelante_echo, HIGH);
     distancia_delantera = tiempo / 59;
     Serial.println(distancia_delantera);
-    seguidordelinealect = digitalRead(seguidor_de_linea);  // Lectura del seguidor de línea
     // Detección de obstáculo (si el objetivo está a menos de 20 cm)
-    if ((distancia_delantera < 20) && (bandera == false)) {
-      digitalWrite(motor_A1, HIGH);
-      digitalWrite(motor_A2, LOW);
-      digitalWrite(motor_B1, LOW);
-      digitalWrite(motor_B2, HIGH);
+    if (distancia_delantera < 60) {
+      digitalWrite(motor_A1, LOW);
+      digitalWrite(motor_A2, HIGH);
+      digitalWrite(motor_B1, HIGH);
+      digitalWrite(motor_B2, LOW);
     }
     // Si está a más de 20 cm que gire (siempre pensando en la bandera) 
-    else if ((distancia_delantera >= 20) && (bandera == false)){
-      digitalWrite(motor_A1, LOW);
+    else if (distancia_delantera >= 20){
+      digitalWrite(motor_A1, HIGH);
       digitalWrite(motor_A2, LOW);
-      digitalWrite(motor_B1, LOW);
-      digitalWrite(motor_B2, HIGH);
+      digitalWrite(motor_B1, HIGH);
+      digitalWrite(motor_B2, LOW);
     }
     //  Si detecté la línea pues retrocedo por unos 2 segundos
     else if (bandera){
@@ -68,7 +63,7 @@ void loop() {
   if (digitalRead(pinDelBotonReLoco)){        //  Si se presionó el botón
     while(digitalRead(pinDelBotonReLoco)){}   //  mantengo a pretado hasta que lo suelte
     if (!yaEspereBro){
-      delay(1000);                              //  Espero 1000ms que serían 1 segundo
+      delay(5000);                              //  Espero 1000ms que serían 1 segundo
       yaEspereBro = true;
       Serial.println("Boton pulsado");
     } else {                     //  Espero 1000ms que serían 1 segundo
